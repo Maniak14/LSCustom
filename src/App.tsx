1c5df4107d2969eb5738ae2601d4f0c1d7d60c26
Maniak14 <doryan.ego@outlook.fr>
2026-01-16 22:34:21 +0100

    Add ScrollToTop component to App for improved navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecruitmentProvider } from "./contexts/RecruitmentContext";
+import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import Tarifs from "./pages/Tarifs";
 import Candidature from "./pages/Candidature";
@@ -25,6 +26,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter basename={basename}>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/tarifs" element={<Tarifs />} />

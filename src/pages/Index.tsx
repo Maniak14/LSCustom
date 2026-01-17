f1a6e8c50cc5ace15a2c7ee3f4808214ddbe5e6a
Maniak14 <doryan.ego@outlook.fr>
2026-01-17 18:27:28 +0100

    Adjust background opacity on Index page to 20 for improved visual effect

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,7 @@ const Index: React.FC = () => {
     <div className="min-h-screen bg-background flex flex-col relative">
       {/* Image de fond avec opacité */}
       <div 
-        className="fixed inset-0 z-0 opacity-25"
+        className="fixed inset-0 z-0 opacity-20"
         style={{
           backgroundImage: 'url(/background.jpg)',
           backgroundSize: 'cover',

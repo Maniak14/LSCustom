d82a9dd191793ffb347b57f4223135a3e9595139
Maniak14 <doryan.ego@outlook.fr>
2026-01-17 14:07:43 +0100

    Update Tarifs component layout to improve responsiveness by adjusting grid settings and centering the pricing grid

--- src/pages/Tarifs.tsx
+++ src/pages/Tarifs.tsx
@@ -92,7 +92,7 @@ const Tarifs: React.FC = () => {
           </div>
 
           {/* Pricing Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto">
             {pricingCategories.map((category, index) => (
               <div
                 key={category.title}
